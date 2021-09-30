853dc35f97aa517e33f50fe8cc62e1d97827d6d4
TianyouCheng <3057580268@qq.com>
2021-09-30 19:39:07 +0800

    💊 Need to sort out
    1. 完成了表格控件的增删
    2. 完成了树形控件的陈列
    3. 上述两点都需要整理文件
    4. 增加了启动界面动画

--- Main.py
+++ Main.py
@@ -1,7 +1,8 @@
-import os,sys
+import os,sys,random,cgitb
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
+from time import sleep
 
 #region 引入窗体及函数
 from UI import *
@@ -30,10 +31,86 @@ class Main_exe(QMainWindow,Ui_MainWindow):
         # 绘图函数
         self.draw_something()
 
+
+# region TabelView控件  需要整理
+        #https://zhuanlan.zhihu.com/p/34532247
+        #https://github.com/PyQt5/PyQt/tree/master/QTreeWidget
+        # 目前实现了删除单行，看需不需要删除多行。
+        self.id=1
+        font = QFont('微软雅黑', 20)
+        font.setBold(True)  # 设置字体加粗
+        # self.tableWidget.horizontalHeader().setFont(font)  # 设置表头字体
+
+        # self.tableWidget.setFrameShape(QFrame.NoFrame)  ##设置无表格的外框
+        self.tableWidget.setSelectionMode(QAbstractItemView.SingleSelection)  # 设置只可以单选，可以使用ExtendedSelection进行多选
+        self.tableWidget.setSelectionBehavior(QAbstractItemView.SelectRows)  # 设置 不可选择单个单元格，只可选择一行。
+        self.tableWidget.setColumnCount(5)  ##设置表格一共有五列
+        self.tableWidget.setEditTriggers(QAbstractItemView.NoEditTriggers)  # 设置表格不可更改
+        self.tableWidget.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)  # 设置第五列宽度自动调整，充满屏幕
+        # 设置表头
+        self.tableWidget.setHorizontalHeaderLabels(
+            ['序号', '姓名', '年龄', '地址','成绩'])
+        self.lines=[]
+
+        # TREEVIEW
+        pitem1=QTreeWidgetItem(self.treeWidget,['Layers'])
+        citem1=QTreeWidgetItem(pitem1,['Mountain'])
+        citem11 = QTreeWidgetItem(citem1, ['Mountain Symbol'])
+        citem11.setIcon(0,QIcon('./UI/icon1.png'))
+        citem2 = QTreeWidgetItem(pitem1, ['Street'])
+        citem22 = QTreeWidgetItem(citem2, ['Street Symbol'])
+        citem22.setIcon(0, QIcon('./UI/icon1.png'))
+        citem3 = QTreeWidgetItem(pitem1, ['Water'])
+        citem33 = QTreeWidgetItem(citem3, ['Water Symbol'])
+        citem33.setIcon(0, QIcon('./UI/icon1.png'))
+        citem4 = QTreeWidgetItem(pitem1, ['City'])
+        citem44 = QTreeWidgetItem(citem4, ['City Symbol'])
+        citem44.setIcon(0, QIcon('./UI/icon1.png'))
+        citem1.setCheckState(0, Qt.Checked)
+        citem2.setCheckState(0, Qt.Checked)
+        citem3.setCheckState(0, Qt.Checked)
+        citem4.setCheckState(0, Qt.Checked)
+        self.treeWidget.header().setVisible(False)
+        self.treeWidget.expandAll()
+
+
+    def TabelViewAdd(self):
+        # Todo 优化2 添加数据
+        row = self.tableWidget.rowCount()
+        self.tableWidget.setRowCount(row + 1)
+        id = str(self.id)
+        name = 'Ana'
+        score = str(random.randint(50, 99))
+        age=str(random.randint(18, 30))
+        add = 'America'
+        self.tableWidget.setItem(row, 0, QTableWidgetItem(id))
+        self.tableWidget.setItem(row, 1, QTableWidgetItem(name))
+        self.tableWidget.setItem(row, 2, QTableWidgetItem(age))
+        self.tableWidget.setItem(row, 3, QTableWidgetItem(add))
+        self.tableWidget.setItem(row, 4, QTableWidgetItem(score))
+        self.id += 1
+        self.lines.append([id, name, age, add, score])
+
+    def TabelViewDel(self):
+        # TODO 优化3 删除当前选中的数据
+        selected_items=self.tableWidget.selectedItems()
+        if len(selected_items)==0:
+            return
+        self.tableWidget.removeRow(self.tableWidget.indexFromItem(selected_items[0]).row())
+# endregion
+
+# region TreeView控件  需要整理
+
+
+
+
     # region 功能函数
     # 绑定信号与槽函数
     def slot_connect(self):
         self.tsButtonEdit.clicked.connect(self.bt_edit_clicked)
+        # TabelView相关
+        self.pushButtonAdd.clicked.connect(self.TabelViewAdd)
+        self.pushButtonDel.clicked.connect(self.TabelViewDel)
 
     # 坐标转换，将事件E的坐标转换到画布坐标上
     def ConvertCor(self,e):
@@ -78,9 +155,60 @@ class Main_exe(QMainWindow,Ui_MainWindow):
     # endregion
 
 
+
+
+class GifSplashScreen(QSplashScreen):
+
+
+    def __init__(self, *args, **kwargs):
+        super(GifSplashScreen, self).__init__(*args, **kwargs)
+        self.movie = QMovie('./UI/img.jpg')
+        self.movie.frameChanged.connect(self.onFrameChanged)
+        self.movie.start()
+
+
+    def onFrameChanged(self, _):
+        self.setPixmap(self.movie.currentPixmap())
+
+
+    def finish(self, widget):
+        self.movie.stop()
+        super(GifSplashScreen, self).finish(widget)
+
+
+
+
+class BusyWindow(QWidget):
+
+
+    def __init__(self, *args, **kwargs):
+        super(BusyWindow, self).__init__(*args, **kwargs)
+        # 模拟耗时操作，一般来说耗时的加载数据应该放到线程
+        for i in range(2):
+            sleep(1)
+            splash.showMessage('加载进度: 100%', Qt.AlignHCenter | Qt.AlignBottom, Qt.white)
+            QApplication.instance().processEvents()
+
+
+        splash.showMessage('初始化完成', Qt.AlignHCenter | Qt.AlignBottom, Qt.white)
+        splash.finish(self)
 # 作为主窗体运行
 if __name__=='__main__':
     myapp=QApplication(sys.argv)
+
+    # 启动界面设置
+    cgitb.enable(format='text')
+
+    global splash
+    splash=GifSplashScreen()
+    splash.show()
+    w = BusyWindow()
+    # w.show()
+
+    splash.showMessage('等待创建界面', Qt.AlignHCenter | Qt.AlignBottom, Qt.white)
+
     myDlg=Main_exe()
     myDlg.show()
     sys.exit(myapp.exec_())
+
+
